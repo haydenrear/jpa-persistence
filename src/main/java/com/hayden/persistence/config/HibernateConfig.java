1fbbfc1d89be983f568c6c18ba308ff46c2f0077
hayden.rear <hayden.rear@gmail.com>
2024-07-25 17:57:01 -0400

    git model

---
+++ src/main/java/com/hayden/persistence/config/HibernateConfig.java
@@ -0,0 +1,9 @@
+package com.hayden.persistence.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HibernateConfig {
+
+
+}
--- src/main/java/com/hayden/persistence/config/JpaConfig.java
+++ src/main/java/com/hayden/persistence/config/JpaConfig.java
@@ -1,11 +1,13 @@
 package com.hayden.persistence.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import java.util.Optional;
 
 @EnableJpaAuditing
+@Configuration
 public class JpaConfig {
 
     AuditorAware<String> auditorAware() {
