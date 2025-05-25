02095bd0e7b4362ddf014ff5fab5e85a194d2a29
hayden.rear <hayden.rear@gmail.com>
2025-05-25 14:54:10 -0400

    updated

--- src/main/java/com/hayden/persistence/config/HibernateConfig.java
+++ src/main/java/com/hayden/persistence/config/HibernateConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.orm.jpa.LocalEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
 import java.util.Optional;
 
--- src/main/java/com/hayden/persistence/config/JpaConfig.java
+++ src/main/java/com/hayden/persistence/config/JpaConfig.java
@@ -1,14 +1,9 @@
 package com.hayden.persistence.config;
 
-import jakarta.persistence.EntityManagerFactory;
-import jakarta.transaction.TransactionManager;
-import org.hibernate.SessionFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.orm.hibernate5.HibernateTransactionManager;
-import org.springframework.transaction.PlatformTransactionManager;
 
 import java.util.Optional;
 
